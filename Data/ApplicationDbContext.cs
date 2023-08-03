d47bb9472191e37accd4fc2d0f79ce04875045fe
NitaRose <rose_nita@outlook.com>
2023-08-03 09:41:07 -0400

    Current update of MusicLibrary.V file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MusicLibraryWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicLibraryWebAPI.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public class DbSet{} Song Songs { get; set; }
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        
+        }
+    }
+}
+
+
